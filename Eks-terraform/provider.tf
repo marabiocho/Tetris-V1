e123f6d4e1732098542af97cd7c3635c70e35901
marabiocho <103324080+marabiocho@users.noreply.github.com>
2024-12-13 00:07:44 -0600

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -2,6 +2,8 @@
 terraform {
   required_providers {
     aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
       source = "hashicorp/aws"
       # version = "~> 5.0" # Optional but recommended in production
     }
@@ -10,21 +12,4 @@ terraform {
       # version = "~> 3.0" # Optional but recommended in production
     }
   }
-}
-
-# Vault Provider Block
-provider "vault" {
-  address = "http://192.168.56.41:8200"
-  token   = "hvs.FOb6crKNmuJHVGfZtVXARioG"
-}
-
-# Data Source to Fetch AWS Credentials from Vault
-data "vault_generic_secret" "aws_credentials" {
-  path = "kv/data/aws/credentials" # Correct path for Vault v2 secrets
-}
-
-provider "aws" {
-  region     = data.vault_generic_secret.aws_credentials.data["region"]
-  access_key = data.vault_generic_secret.aws_credentials.data["aws_access_key_id"]
-  secret_key = data.vault_generic_secret.aws_credentials.data["aws_secret_access_key"]
 }
